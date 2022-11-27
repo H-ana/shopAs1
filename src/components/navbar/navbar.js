af797bd013ccd2f31a36126e5f49c4b2036e955c
Rahul v s <rahulvs037@gmail.com>
2022-11-27 12:05:23 +0530

    navbar 2

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,20 +4,24 @@ import Logo from '../logo';
 import { FaCaretDown } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa';
 import { FaShoppingCart } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
 function Navbar(){
     return (
         <div className='navbar'>
-<ul>
-    <li><a href='#'>MEN <FaCaretDown/></a></li>
-    <li><a href='#'>WOMEN <FaCaretDown/></a></li>
-    <li><a href='#'>KIDS <FaCaretDown/></a></li>
-    <li><Logo/></li>
-    <li><a href='#'>HOME AND KITCHEN <FaCaretDown/></a></li>
-    
-    <li><a href='#'><FaHeart/></a></li>
-    <li><a href='#'><FaShoppingCart/></a></li>
-</ul>
+            <header>
+    <ul>
+        <li><a href='#'><div><h2><CgProfile/></h2><h3>profile</h3></div></a></li>
+        <li><a href='#'>MEN <FaCaretDown/></a></li>
+        <li><a href='#'>WOMEN <FaCaretDown/></a></li>
+        <li><a href='#'>KIDS <FaCaretDown/></a></li>
+       <li><Logo/></li>
+        <li><a href='#'>HOME AND KITCHEN <FaCaretDown/></a></li>
+        <li><a href='#'><div><h2><FaHeart/></h2><h3>wishlist</h3></div></a></li>
+        <li><a href='#'><div><h2><FaShoppingCart/></h2><h3>cart</h3></div></a></li>
+    </ul>
+    </header>
         </div>
+
     )
 }
 export default Navbar;
